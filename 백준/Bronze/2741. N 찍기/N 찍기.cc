08cd83a2bf92145e653077897bbe20c4239d87f4
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-12 19:55:45 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	unsigned int input;
+	cin >> input;
+
+	for (int i = 1;i <= input;i++) cout << i << "\n";
+	
+	return 0;
+}
