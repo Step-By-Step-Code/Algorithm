8b8bc36b29bd748e86a05771603c063e86e0c2dd
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-13 10:34:12 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	vector<int> vec;
+	int input1, input2, input3;
+	while (cin >> input2 >> input3) cout << input2 + input3 << "\n";
+
+
+	return 0;
+}
