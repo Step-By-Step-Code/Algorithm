3a2e5d1111e89d6ab4080c60bb6e937079d74035
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-13 10:39:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	char input1;
+	cin >> input1;
+	cout <<  (int)input1;
+
+
+	return 0;
+}
