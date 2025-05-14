802fc950199a9879288dc82b8db7b6557ef31c0d
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-14 13:20:43 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	long long input1, input2, input3;
+	cin >> input1;
+	cin >> input2; 
+	cin >> input3;
+	cout << input1 + input2 + input3 << "\n";
+
+	return 0;
+}
