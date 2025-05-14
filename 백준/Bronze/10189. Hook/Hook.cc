558d7f9af205433ae61a276bd30777ccd501e74f
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-14 13:35:40 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	cout << "#  # #### #### #  #" << "\n";
+	cout << "#### #  # #  # # #" << "\n";
+	cout << "#### #  # #  # # #" << "\n";
+	cout << "#  # #### #### #  #" << "\n";
+	return 0;
+}
