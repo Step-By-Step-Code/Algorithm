21b6c6831da1f66870f0822a725051cfd4e05596
Step-By-Step-Code <ksmin6450@gmail.com>
2025-05-14 13:34:04 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int input1;
+	cin >> input1;
+	if (input1 == 0) cout << "YONSEI";
+	else cout << "Leading the Way to the Future";
+	return 0;
+}
