236acc0633b4d9735ff460b89003c123452a4f6f
Step-By-Step-Code <ksmin6450@gmail.com>
2026-04-01 11:17:02 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, C, D, E;
+    cin >> A >> B >> C >> D >> E;
+
+    int result = A + B + C + D + E;
+    cout << result << "\n";
+    return 0;
+}
