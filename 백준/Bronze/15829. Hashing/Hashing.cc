ea208f352eaad8e566827eb5f4672233b59bcac2
Step-By-Step-Code <ksmin6450@gmail.com>
2026-04-06 16:00:32 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 int main() {
     int input_1;
     cin >> input_1;
-    vector<char> v(input_1);
-    unsigned long long result = 0;
 
-    for(int i=0; i < input_1 ;i++){
+    vector<char> v(input_1);
+    const unsigned long long MOD = 1234567891;
+
+    unsigned long long result = 0;
+    unsigned long long power = 1;   // 31^0
+
+    for(int i = 0; i < input_1; i++){
         cin >> v[i];
-        result += (static_cast<unsigned long long>(v[i] -'a' + 1) * static_cast<unsigned long long>(pow(31, i)));
-        result = result % 1234567891;
+
+        result = (result + (v[i] - 'a' + 1) * power) % MOD;
+        power = (power * 31) % MOD;   // 다음 거듭제곱
     }
-    
+
     cout << result << endl;
     return 0;
 }
