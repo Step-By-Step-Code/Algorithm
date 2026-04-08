0a47273c7ebd1c28f2ec83fd5f7c74993286f8af
Step-By-Step-Code <ksmin6450@gmail.com>
2026-04-08 10:34:00 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string input_1;
+    cin >> input_1;
+
+    int result = input_1.size();
+    
+    cout << result << endl;
+    return 0;
+}
